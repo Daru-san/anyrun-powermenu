2fc7001806c9ba5d32fe7e874c54cd62a32260d2
Daru <mica.tui@proton.me>
2024-06-25 13:40:57 +0200

    flake: init

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Color picker for wayland written in rust";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        toolchain = pkgs.rustPlatform;
+      in rec
+      {
+        packages.default = toolchain.buildRustPackage {
+          pname = "anyrun-powermenu";
+          version = "unstable-2024-06-25";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (with toolchain; [
+              cargo
+              rustc
+              rustLibSrc
+            ])
+            clippy
+            rustfmt
+            pkg-config
+          ];
+
+          # Specify the rust-src path (many editors rely on this)
+          RUST_SRC_PATH = "${toolchain.rustLibSrc}";
+        };
+      }
+    );
+}
